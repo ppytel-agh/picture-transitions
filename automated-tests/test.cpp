03eadbffc12c3da9599a88e3a82feb4bbe956c7a
Pawel Pytel <ppytel@student.agh.edu.pl>
2022-06-04 17:49:45 +0200

    written test for animation-generator build but it does not compile

--- automated-tests/test.cpp
+++ automated-tests/test.cpp
@@ -1,6 +1,11 @@
 #include "pch.h"
 
+#include <animation-generator-app.h>
+
 TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+	EXPECT_EQ(1, 1);
+	EXPECT_TRUE(true);
+}
+TEST(AnimationGenerator, Build) {
+	AnimationGeneratorApp app;
 }
