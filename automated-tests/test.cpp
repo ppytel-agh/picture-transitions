8bdcbe21ea7e127e8e8f8347f1b9ceef02f695e2
Pawel Pytel <ppytel@student.agh.edu.pl>
2022-06-04 18:38:59 +0200

    created project animation-generator-lib to export code for testing

--- automated-tests/test.cpp
+++ automated-tests/test.cpp
@@ -1,11 +1,11 @@
 #include "pch.h"
 
-#include <animation-generator-app.h>
+//#include "../animation-generator-lib/animation-generator-app.h"
 
 TEST(TestCaseName, TestName) {
 	EXPECT_EQ(1, 1);
 	EXPECT_TRUE(true);
 }
-TEST(AnimationGenerator, Build) {
-	AnimationGeneratorApp app;
-}
+//TEST(AnimationGenerator, Build) {
+//	AnimationGeneratorApp app;
+//}
