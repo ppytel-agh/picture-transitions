1c83cdedcbce23529e4ddc58f17698bb3231182c
M4yfl0w3r <mr.mayflower15@gmail.com>
2022-06-05 20:53:03 +0200

    fgdfsf

--- animation-generator-lib/animation-generator-app.cpp
+++ animation-generator-lib/animation-generator-app.cpp
@@ -1,12 +1,55 @@
 #include "animation-generator-app.h"
 
-bool AnimationGeneratorApp::OnInit() {
+bool AnimationGeneratorApp::OnInit() 
+{
 	mainFrame* frame = new mainFrame(NULL);
 	frame->Show(true);
 	SetTopWindow(frame);
 	return true;
 }
 
-int AnimationGeneratorApp::OnExit() {
+int AnimationGeneratorApp::OnExit() 
+{
 	return 0;
 }
+
+void AnimationGeneratorApp::onReset(wxCommandEvent& event)
+{
+	// TODO: Implement onReset
+}
+
+void AnimationGeneratorApp::onLoadInitFrame(wxCommandEvent& event)
+{
+	// TODO: Implement onLoadInitFrame
+}
+
+void AnimationGeneratorApp::onLoadLastFrame(wxCommandEvent& event)
+{
+	// TODO: Implement onLoadLastFrame
+}
+
+void AnimationGeneratorApp::onTransitionChoice(wxCommandEvent& event)
+{
+	// TODO: Implement onTransitionChoice
+}
+
+void AnimationGeneratorApp::onFrameRateEnter(wxCommandEvent& event)
+{
+	// TODO: Implement onFrameRateEnter
+}
+
+void AnimationGeneratorApp::onGenerateFrame(wxCommandEvent& event)
+{
+	// TODO: Implement onGenerateFrame
+}
+
+void AnimationGeneratorApp::onScroll(wxScrollEvent& event)
+{
+	// TODO: Implement onScroll
+}
+
+void AnimationGeneratorApp::onAnimationSave(wxCommandEvent& event)
+{
+	// TODO: Implement onAnimationSave
+}
+
--- animation-generator-lib/animation-generator-app.h
+++ animation-generator-lib/animation-generator-app.h
@@ -3,8 +3,20 @@
 #include <wx/wx.h>
 #include "mainFrame.h"
 
-class AnimationGeneratorApp : public wxApp {
+class AnimationGeneratorApp : public wxApp 
+{
+
 public:
 	virtual bool OnInit();
 	virtual int OnExit();
+
+protected:
+	void onReset(wxCommandEvent& event);
+	void onLoadInitFrame(wxCommandEvent& event);
+	void onLoadLastFrame(wxCommandEvent& event);
+	void onTransitionChoice(wxCommandEvent& event);
+	void onFrameRateEnter(wxCommandEvent& event);
+	void onGenerateFrame(wxCommandEvent& event);
+	void onScroll(wxScrollEvent& event);
+	void onAnimationSave(wxCommandEvent& event);
 };
