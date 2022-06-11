1ba30fbd244b088e1099122b884cc3cd08bc4d9d
Pawel Pytel <ppytel@student.agh.edu.pl>
2022-06-11 15:40:54 +0200

    parameter name fix

--- animation-generator-lib/simple-animation-generator.cpp
+++ animation-generator-lib/simple-animation-generator.cpp
@@ -1,6 +1,6 @@
 #include "simple-animation-generator.h"
 
-void generateAnimation(const GraphicsBuffer& startKeyframe, const GraphicsBuffer& endKeyframe, AnimationFrameFillerInterface& frameFiller, Size animationFrameSize, std::vector<float> frameNormsSeries, std::function<void(unsigned int, unsigned int)> generatedFrameCallback, std::function<void(std::vector<GraphicsBuffer>)> generatedAnimationCallback, Pixel animationBackground)
+void generateAnimation(const GraphicsBuffer& startKeyframe, const GraphicsBuffer& endKeyframe, AnimationFrameFillerInterface& frameFiller, Size animationFrameSize, std::vector<float> frameNormsSeries, std::function<void(unsigned int, unsigned int)> generatedFrameCallback, std::function<void(std::vector<GraphicsBuffer>)> generatedAnimationCallback, Pixel animationBackgroundColour)
 {
 	std::vector<GraphicsBuffer> animationFrames;
 	int numberOfFrames = frameNormSeries.size();
