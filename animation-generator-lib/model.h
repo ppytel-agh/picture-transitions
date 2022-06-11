7d9d82170d34d795316cb0e09d824ddfd602d66a
Pawel Pytel <ppytel@student.agh.edu.pl>
2022-06-11 14:31:44 +0200

    written documentation for model class

--- animation-generator-lib/model.h
+++ animation-generator-lib/model.h
@@ -8,16 +8,56 @@
 
 class Model {
 public:
+	/**
+	* empties current field values
+	*/
 	void reset();
-	void setStartKeyframe(GraphicsBuffer&&);
+	/**
+	* model claims ownership of start keyframe buffer
+	* @param nskf new start keyframe
+	*/
+	void setStartKeyframe(GraphicsBuffer&& nskf);
+	/*
+	* If start keyframe is not set, then empty buffer is returned
+	* @return copy of start keyframe
+	*/
 	GraphicsBuffer getStartKeyframe() const;
+	/**
+	* Returns {0,0} is start keyframe is not set
+	* @return size of start keyframe
+	*/
 	std::pair<unsigned int, unsigned int> getStartKeyframeSize() const;
-	void setEndKeyframe(GraphicsBuffer&&);
+	/**
+	* model claims ownership of end keyframe buffer
+	* @param nekf new end keyframe
+	*/
+	void setEndKeyframe(GraphicsBuffer&& nekf);
+	/*
+	* If end keyframe is not set, then empty buffer is returned
+	* @return copy of end keyframe
+	*/
 	GraphicsBuffer getEndKeyframe() const;
+	/**
+	* Returns {0,0} is end keyframe is not set
+	* @return size of end keyframe
+	*/
 	std::pair<unsigned int, unsigned int> getEndKeyframeSize() const;
 	void setAnimationFrames(std::vector<GraphicsBuffer>&&);
+	/**
+	* If animation is not set, then 0 is returned
+	* @return number of animation frames
+	*/
 	unsigned int getNumberOfAnimationFrames() const;
-	GraphicsBuffer getAnimationFrame(unsigned int) const;
+	/**
+	* @return empty buffer if frame with provided index does not exist
+	* @param i frame index
+	* @return copy of selected frame buffer
+	*/
+	GraphicsBuffer getAnimationFrame(unsigned int i) const;
+	/**
+	* If animation is not set, then ampty vector is returned
+	* @return copy of all animation frames
+	*/
 	std::vector<GraphicsBuffer> getAllAnimationFrames() const;
 private:
 	std::unique_ptr<GraphicsBuffer> startKeyframe;
