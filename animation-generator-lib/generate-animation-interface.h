3e22d417323c5267f5362bdff60d1c77453ea03b
Pawel Pytel <ppytel@student.agh.edu.pl>
2022-06-11 15:39:41 +0200

    corrected animation generator interface

--- animation-generator-lib/generate-animation-interface.h
+++ animation-generator-lib/generate-animation-interface.h
@@ -1,9 +1,11 @@
 #pragma once
 
 #include <vector>
+#include <functional>
 #include "graphics-buffer.h"
+#include "animation-frame-filler-interface.h"
 
 class GenerateAnimationInterface {
 public:
-	virtual std::vector<GraphicsBuffer> generateAnimation(const GraphicsBuffer& startKeyframe, const GraphicsBuffer& endKeyframe, AnimationFrameFillerInterface& frameFiller, Size animationFrameSize, std::vector<float> frameNormsSeries, Pixel animationBackgroundColour = {}) = 0;
+	virtual void generateAnimation(const GraphicsBuffer& startKeyframe, const GraphicsBuffer& endKeyframe, AnimationFrameFillerInterface& frameFiller, Size animationFrameSize, std::vector<float> frameNormsSeries, std::function<void(unsigned int, unsigned int)> generatedFrameCallback, std::function<void(std::vector<GraphicsBuffer>)> generatedAnimationCallback, Pixel animationBackground = {}) = 0;
 };
---
+++ animation-generator-lib/simple-animation-generator.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "generate-animation-interface.h"
+#include "norm-calc.h"
+
+class SimpleAnimationGenerator: public GenerateAnimationInterface {
+public:
+
+
+	// Inherited via GenerateAnimationInterface
+	virtual void generateAnimation(const GraphicsBuffer& startKeyframe, const GraphicsBuffer& endKeyframe, AnimationFrameFillerInterface& frameFiller, Size animationFrameSize, std::vector<float> frameNormsSeries, std::function<void(unsigned int, unsigned int)> generatedFrameCallback, std::function<void(std::vector<GraphicsBuffer>)> generatedAnimationCallback, Pixel animationBackground = {}) override;
+
+};
