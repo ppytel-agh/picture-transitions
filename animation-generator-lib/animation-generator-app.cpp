70c6ce78fb235ba5a3201f1d976b2c3912ca6c3b
M4yfl0w3r <mr.mayflower15@gmail.com>
2022-06-11 21:25:16 +0200

    x

--- animation-generator-lib/animation-generator-app.cpp
+++ animation-generator-lib/animation-generator-app.cpp
@@ -1,12 +1,72 @@
 #include "animation-generator-app.h"
 
-bool AnimationGeneratorApp::OnInit() {
-	wxFrame* mainFrame = new wxFrame(NULL, -1, "Animation Generator");
-	mainFrame->Show(true);
-	SetTopWindow(mainFrame);
+bool AnimationGeneratorApp::OnInit() 
+{
+	wxFrame* frame = new Frame(NULL);
+	frame->Show(true);
+	SetTopWindow(frame);
 	return true;
 }
 
-int AnimationGeneratorApp::OnExit() {
+int AnimationGeneratorApp::OnExit() 
+{
 	return 0;
 }
+
+Frame::Frame(wxWindow* parent)
+	: mainFrame(parent)
+{
+
+}
+
+void Frame::onReset(wxCommandEvent& event)
+{
+	// TODO: Implement onReset
+}
+
+void Frame::onLoadInitFrame(wxCommandEvent& event)
+{
+	wxFileDialog* WxOpenFirstFrameDialog(new wxFileDialog(this, _("Choose a file"), _(" "), _(""), _("JPEG files (*.jpg)|*.jpg|PNG files (*.png)|*.png|Bitmap (*.bmp)|*.bmp"), wxFD_OPEN|wxFD_FILE_MUST_EXIST));
+
+	if (WxOpenFirstFrameDialog->ShowModal() == wxID_OK)
+	{
+	}
+}
+
+void Frame::onLoadLastFrame(wxCommandEvent& event)
+{
+	wxFileDialog* WxOpenLastFrameDialog(new wxFileDialog(this, _("Choose a file"), _(" "), _(""), _("JPEG files (*.jpg)|*.jpg|PNG files (*.png)|*.png|Bitmap (*.bmp)|*.bmp"), wxFD_OPEN | wxFD_FILE_MUST_EXIST));
+
+	if (WxOpenLastFrameDialog->ShowModal() == wxID_OK)
+	{
+	}
+
+}
+
+void Frame::onTransitionChoice(wxCommandEvent& event)
+{
+	// TODO: Implement onTransitionChoice
+}
+
+void Frame::onFrameRateEnter(wxCommandEvent& event)
+{
+	// TODO: Implement onFrameRateEnter
+}
+
+void Frame::onGenerateFrame(wxCommandEvent& event)
+{
+	// TODO: Implement onGenerateFrame
+}
+
+void Frame::onScroll(wxScrollEvent& event)
+{
+	// TODO: Implement onScroll
+}
+
+void Frame::onAnimationSave(wxCommandEvent& event)
+{
+	wxFileDialog WxSaveAnimationDialog(new wxFileDialog(this, _("Choose a file"), _(" "), _(""), _("JPEG files (*.jpg)|*.jpg|PNG files (*.png)|*.png|Bitmap (*.bmp)|*.bmp"), wxFD_SAVE | wxFD_OVERWRITE_PROMPT));
+
+}
+
+
--- animation-generator-lib/animation-generator-app.h
+++ animation-generator-lib/animation-generator-app.h
@@ -1,9 +1,30 @@
 #pragma once
 
 #include <wx/wx.h>
+#include <memory>
+#include "mainFrame.h"
 
-class AnimationGeneratorApp : public wxApp {
+class AnimationGeneratorApp : public wxApp 
+{
+
 public:
 	virtual bool OnInit();
 	virtual int OnExit();
+};
+
+class Frame : public mainFrame
+{
+
+public:
+	void onReset(wxCommandEvent& event) override;
+	void onLoadInitFrame(wxCommandEvent& event) override;
+	void onLoadLastFrame(wxCommandEvent& event) override;
+	void onTransitionChoice(wxCommandEvent& event) override;
+	void onFrameRateEnter(wxCommandEvent& event) override;
+	void onGenerateFrame(wxCommandEvent& event) override;
+	void onScroll(wxScrollEvent& event) override;
+	void onAnimationSave(wxCommandEvent& event) override;
+
+public:
+	Frame(wxWindow* parent);
 };
