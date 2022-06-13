8c82d06c4b08f43eb556081f08ade9dd0cc44354
Pawel Pytel <ppytel@student.agh.edu.pl>
2022-06-13 19:53:22 +0200

    added few tests to graphics buffer

--- unit-tests/graphics-buffer-tests.cpp
+++ unit-tests/graphics-buffer-tests.cpp
@@ -14,6 +14,64 @@ namespace unittests
 	TEST_CLASS(GraphicsBufferTests)
 	{
 	public:
+		TEST_METHOD(initialization) {
+			GraphicsBuffer buffer({ 2, 1 }, { 127, 0, 68 });
+			Assert::AreEqual(2, buffer.GetSize().width);
+			Assert::AreEqual(1, buffer.GetSize().height);
+			Assert::AreEqual({ 127, 0, 68, 127, 0, 68 }, buffer.getSubpixelValues());
+		}
+		TEST_METHOD(copyModification) {
+			GraphicsBuffer buffer({ 2, 2 });
+			GraphicsBuffer copy(buffer);
+			Assert::IsTrue(buffer == copy);
+			copy.setSubpixelValues({ 1, 1, 1 });
+			Assert::IsFalse(buffer == copy);
+		}
+		TEST_METHOD(emptyBuffer) {
+			{
+				GraphicsBuffer buffer({ 2, 2 });
+				Assert::IsFalse(buffer.IsEmpty());
+			}
+			{
+				GraphicsBuffer buffer({ 2, 0 });
+				Assert::IsTrue(buffer.IsEmpty());
+			}
+			{
+				GraphicsBuffer buffer({ 0, 2 });
+				Assert::IsTrue(buffer.IsEmpty());
+			}
+		}
+		TEST_METHOD(moveSemantics) {
+			GraphicsBuffer buffer({ 2, 2 });
+			GraphicsBuffer copyOfOriginal(buffer);
+			GraphicsBuffer moved = std::move(buffer);
+			Assert::IsTrue(buffer.IsEmpty());
+			Assert::IsFalse(copyOfOriginal == buffer);
+			Assert::IsTrue(moved == copyOfOriginal);
+		}
+		TEST_METHOD(receivedSubpixelsCopyModification) {
+			GraphicsBuffer buffer({ 2, 2 }, {0, 0, 0});
+			std::vector<unsigned char> subpixels = buffer.getSubpixelValues();
+			subpixels[0] = 128;
+			Assert::IsFalse(subpixels == buffer.getSubpixelValues());
+		}
+		TEST_METHOD(puttedSubpixelsCopyModification) {
+			GraphicsBuffer buffer({ 2, 1 });
+			std::vector<unsigned char> subpixels{ 128, 0, 128, 128, 0, 128};
+			buffer.setSubpixelValues(subpixels);
+			subpixels[0] = 255;
+			Assert::IsTrue(subpixels == buffer.getSubpixelValues());
+		}
+		TEST_METHOD(puttedSubpixelsOffset) {
+			GraphicsBuffer buffer({ 2, 1 }, {128, 0, 255});
+			buffer.setSubpixelValues({ 64, 20 }, 2);
+			Assert::AreEqual({ 128, 0, 64, 20, 0, 255 }, buffer.getSubpixelValues());
+		}
+		TEST_METHOD(puttedSubpixelsOverflow) {
+			GraphicsBuffer buffer({ 1, 1 });
+			buffer.setSubpixelValues({ 64, 20, 5, 102, 206 }, 5);
+			Assert::AreEqual({ 64, 20, 5 }, buffer.getSubpixelValues());
+		}
 		TEST_METHOD(comparisonTest) {
 			{
 				GraphicsBuffer a({ 5, 5 });
@@ -26,8 +84,8 @@ namespace unittests
 				Assert::IsFalse(a == b);
 			}
 			{
-				GraphicsBuffer a({ 5, 5 }, {255, 255, 255});
-				GraphicsBuffer b({ 5, 4 }, {255, 255, 254});
+				GraphicsBuffer a({ 5, 5 }, { 255, 255, 255 });
+				GraphicsBuffer b({ 5, 4 }, { 255, 255, 254 });
 				Assert::IsFalse(a == b);
 			}
 		}
@@ -37,7 +95,7 @@ namespace unittests
 			source.setSubpixelValues(sourcebmp);
 			GraphicsBuffer expectedResult({ 150, 100 });
 			source.setSubpixelValues(testcase1bmp);
-			GraphicsBuffer section = source.createSection({17, 19}, {87, 54});
+			GraphicsBuffer section = source.createSection({ 17, 19 }, { 87, 54 });
 			Assert::IsTrue(section == expectedResult);
 		}
 
@@ -46,7 +104,7 @@ namespace unittests
 			source.setSubpixelValues(sourcebmp);
 			GraphicsBuffer expectedResult({ 150, 100 });
 			source.setSubpixelValues(testcase2bmp);
-			GraphicsBuffer section = source.createSection({ -18, -27 }, {95, 63}, {0, 255, 255});
+			GraphicsBuffer section = source.createSection({ -18, -27 }, { 95, 63 }, { 0, 255, 255 });
 			Assert::IsTrue(section == expectedResult);
 		}
 
