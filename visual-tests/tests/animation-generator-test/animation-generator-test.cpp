d0db740bf82cad0cc2ad33a3b97af8ca7d04e6fa
Pawel Pytel <ppytel@student.agh.edu.pl>
2022-06-18 16:15:27 +0200

    [05] added size change to test to check fullproofness of filling process

--- visual-tests/tests/animation-generator-test/animation-generator-test.cpp
+++ visual-tests/tests/animation-generator-test/animation-generator-test.cpp
@@ -7,13 +7,16 @@ void AnimationGeneratorTest::initializeUI(wxFrame* parentFrame)
 	wxPanel* m_panel2;
 	wxPanel* m_panel3;
 	wxTextCtrl* m_textCtrl1;
+	wxBoxSizer* whSizer;
+	wxTextCtrl* widthCtrl;
+	wxTextCtrl* heightCtrl;
 	wxButton* m_button1;
 	wxButton* m_button2;
 	wxButton* m_button3;
 	wxScrolledWindow* m_scrolledWindow1;
 	wxStaticText* m_staticText1;
 
-	window = new wxFrame(parentFrame, wxID_ANY, wxEmptyString, wxDefaultPosition, wxSize(530, 600), wxDEFAULT_FRAME_STYLE | wxTAB_TRAVERSAL);
+	window = new wxFrame(parentFrame, wxID_ANY, wxEmptyString, wxDefaultPosition, wxSize(530, 700), wxDEFAULT_FRAME_STYLE | wxTAB_TRAVERSAL);
 
 	window->SetSizeHints(wxDefaultSize, wxDefaultSize);
 
@@ -32,13 +35,23 @@ void AnimationGeneratorTest::initializeUI(wxFrame* parentFrame)
 
 	bSizer1->Add(bSizer2, 0, wxEXPAND, 5);
 
+	whSizer = new wxBoxSizer(wxHORIZONTAL);
+
+	widthCtrl = new wxTextCtrl(window, wxID_ANY, "240", wxDefaultPosition, wxDefaultSize, 0);
+	whSizer->Add(widthCtrl, 0, wxALL | wxEXPAND, 5);
+
+	heightCtrl = new wxTextCtrl(window, wxID_ANY, "135", wxDefaultPosition, wxDefaultSize, 0);
+	whSizer->Add(heightCtrl, 0, wxALL | wxEXPAND, 5);
+
+	bSizer1->Add(whSizer, 0, wxEXPAND, 5);
+
 	m_textCtrl1 = new wxTextCtrl(window, wxID_ANY, wxEmptyString, wxDefaultPosition, wxDefaultSize, 0);
 	bSizer1->Add(m_textCtrl1, 0, wxALL | wxEXPAND, 5);
 
 	m_button1 = new wxButton(window, wxID_ANY, wxT("Generate"), wxDefaultPosition, wxDefaultSize, 0);
 	bSizer1->Add(m_button1, 0, wxALL, 5);
 
-	m_panel3 = new wxPanel(window, wxID_ANY, wxDefaultPosition, wxSize(240, 135), wxTAB_TRAVERSAL);
+	m_panel3 = new wxPanel(window, wxID_ANY, wxDefaultPosition, wxSize(300, 175), wxTAB_TRAVERSAL);
 	m_panel3->SetBackgroundColour(wxSystemSettings::GetColour(wxSYS_COLOUR_ACTIVECAPTION));
 
 	bSizer1->Add(m_panel3, 0, wxALL, 5);
@@ -94,8 +107,10 @@ void AnimationGeneratorTest::initializeUI(wxFrame* parentFrame)
 	std::vector<GraphicsBuffer>** frames = new std::vector<GraphicsBuffer>*;
 	*frames = new std::vector<GraphicsBuffer>;
 	int* frameCounter = new int{};
+	Size* frameSize = new Size{  };
 
 	std::function<void()>* printFrame = new std::function<void()>{ [=]() {
+		m_panel3->ClearBackground();
 		int frameNo = *frameCounter;
 		GraphicsBuffer frameToPrint = (**frames)[frameNo];
 		{
@@ -104,7 +119,7 @@ void AnimationGeneratorTest::initializeUI(wxFrame* parentFrame)
 			wxMemoryDC memDC2;
 			memDC2.SelectObject(memoryBitmap2);
 			wxClientDC dc2(m_panel3);
-			dc2.Blit(0, 0, 240, 135, &memDC2, 0, 0);
+			dc2.Blit(0, 0, frameSize->width, frameSize->height, &memDC2, 0, 0);
 		}
 		std::stringstream ss;
 		ss << "frame no : " << (*frameCounter) + 1 << " / " << (**frames).size();
@@ -131,23 +146,44 @@ void AnimationGeneratorTest::initializeUI(wxFrame* parentFrame)
 
 	std::function<void(wxCommandEvent&)>* generateFun = new std::function<void(wxCommandEvent&)>{ [=](wxCommandEvent&) {
 			std::vector<float> norms;
-			wxString textValue = m_textCtrl1->GetValue();
-			std::string parsedText(textValue);
-			std::stringstream ss(parsedText);
-			while (!ss.eof()) {
-				float norm;
-				ss >> norm;
-				norms.push_back(norm);
+			{
+				wxString textValue = m_textCtrl1->GetValue();
+				std::string parsedText(textValue);
+				std::stringstream ss(parsedText);
+				while (!ss.eof()) {
+					float norm;
+					ss >> norm;
+					norms.push_back(norm);
+				}
 			}
-			for (auto panel : *outputPanels) {
-				delete panel;
+			unsigned int width;
+			{
+				wxString textValue = widthCtrl->GetValue();
+				std::string parsedText(textValue);
+				std::stringstream ss(parsedText);
+				ss >> width;
 			}
+			if (width < 0 || width > 1000) {
+				return;
+			}
+			unsigned int height;
+			{
+				wxString textValue = heightCtrl->GetValue();
+				std::string parsedText(textValue);
+				std::stringstream ss(parsedText);
+				ss >> height;
+			}
+			if (height < 0 || width > 1000) {
+				return;
+			}
+			frameSize->width = width;
+			frameSize->height = height;
 			outputPanels->erase(outputPanels->begin(), outputPanels->end());
 			std::vector<GraphicsBuffer> generatedAnimation = animationGenerator->generateAnimation(
 				*startKeyframeBuffer,
 				*endKeyframeBuffer,
 				*mockFiller,
-				{240, 135},
+				*frameSize,
 				norms,
 				nullptr
 			);
