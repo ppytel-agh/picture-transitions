a0ec556b65c9fc479b791458697f6c421f9d044f
Pawel Pytel <ppytel@student.agh.edu.pl>
2022-06-19 08:09:43 +0200

    [08] reworked transitions manager to store references

--- unit-tests/transitions-manager-tests.cpp
+++ unit-tests/transitions-manager-tests.cpp
@@ -21,14 +21,14 @@ namespace unittests
 		private:
 			char token;
 		};
-		
+
 		TEST_METHOD(getNames)
 		{
 			MockTransition transitionA('A');
 			MockTransition transitionB('B');
 			TransitionsManager manager({
-				{L"przejœcie A", &transitionA},
-				{L"przejœcie B", &transitionB}
+				{L"przejœcie A", transitionA},
+				{L"przejœcie B", transitionB}
 				});
 			std::vector < std::wstring> expected{ L"przejœcie A", L"przejœcie B" };
 			Assert::IsTrue(manager.getTransitionNames() == expected);
@@ -38,8 +38,8 @@ namespace unittests
 			MockTransition transitionA('A');
 			MockTransition transitionB('B');
 			TransitionsManager manager({
-				{L"przejœcie A", &transitionA},
-				{L"przejœcie B", &transitionB}
+				{L"przejœcie A", transitionA},
+				{L"przejœcie B", transitionB}
 				});
 			Assert::IsTrue(manager.transitionExists(0));
 			Assert::IsTrue(manager.transitionExists(1));
@@ -52,14 +52,34 @@ namespace unittests
 			MockTransition transitionA('A');
 			MockTransition transitionB('B');
 			TransitionsManager manager({
-				{L"przejœcie A", &transitionA},
-				{L"przejœcie B", &transitionB}
+				{L"przejœcie A", transitionA},
+				{L"przejœcie B", transitionB}
 				});
-			Assert::IsTrue(manager.getTransitionFiller(-1) == nullptr);
-			Assert::IsTrue(manager.getTransitionFiller(0) == &transitionA);
-			Assert::IsTrue(manager.getTransitionFiller(1) == &transitionB);
-			Assert::IsTrue(manager.getTransitionFiller(2) == nullptr);
-			Assert::IsTrue(manager.getTransitionFiller(999) == nullptr);
+			{
+				AnimationFrameFillerInterface& fillerRef = manager.getTransitionFiller(-1);
+				bool isEmpty = manager.fillerIsEmpty(fillerRef);
+				Assert::AreEqual(true, isEmpty);
+			}
+			{
+				AnimationFrameFillerInterface& fillerRef = manager.getTransitionFiller(2);
+				bool isEmpty = manager.fillerIsEmpty(fillerRef);
+				Assert::AreEqual(true, isEmpty);
+			}
+			{
+				AnimationFrameFillerInterface& fillerRef = manager.getTransitionFiller(999);
+				bool isEmpty = manager.fillerIsEmpty(fillerRef);
+				Assert::AreEqual(true, isEmpty);
+			}
+			{
+				MockTransition& mockFiller = dynamic_cast<MockTransition&>(manager.getTransitionFiller(0));
+				bool isEmpty = manager.fillerIsEmpty(mockFiller);
+				Assert::AreEqual(false, isEmpty);
+				Assert::AreEqual('A', mockFiller.getToken());
+			}
+			MockTransition& mockFiller = dynamic_cast<MockTransition&>(manager.getTransitionFiller(1));
+			bool isEmpty = manager.fillerIsEmpty(mockFiller);
+			Assert::AreEqual(false, isEmpty);
+			Assert::AreEqual('B', mockFiller.getToken());
 		}
 	};
 }
