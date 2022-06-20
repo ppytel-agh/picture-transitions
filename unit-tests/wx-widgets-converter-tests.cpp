4e5902d6775b04dbae8fafe78ef42f1f5b9c0fd3
Pawel Pytel <ppytel@student.agh.edu.pl>
2022-06-20 13:30:01 +0200

    [23] converter returns single pixel image if converted buffer is empty invalid image is converted to empty buffer

--- unit-tests/wx-widgets-converter-tests.cpp
+++ unit-tests/wx-widgets-converter-tests.cpp
@@ -17,9 +17,41 @@ namespace unittests
 			wxImage imageFromEmpty = WxWidgetsBufferConverter::convertBufferToWxImage(emptyBuffer);
 			Assert::AreEqual(1, imageFromEmpty.GetWidth());
 			Assert::AreEqual(1, imageFromEmpty.GetHeight());
-			Assert::AreEqual((unsigned char)0, imageFromEmpty.GetData()[0]);
-			Assert::AreEqual((unsigned char)0, imageFromEmpty.GetData()[1]);
-			Assert::AreEqual((unsigned char)0, imageFromEmpty.GetData()[2]);
+			Assert::AreEqual((unsigned char)0, imageFromEmpty.GetRed(0, 0));
+			Assert::AreEqual((unsigned char)0, imageFromEmpty.GetGreen(0, 0));
+			Assert::AreEqual((unsigned char)0, imageFromEmpty.GetBlue(0, 0));
+			Assert::AreEqual((unsigned char)0, imageFromEmpty.GetAlpha(0, 0));
+		}
+
+		TEST_METHOD(nonemptyBuffer) {
+			GraphicsBuffer nonemptyBuffer({ 2, 3 });
+			wxImage imageFromEmpty = WxWidgetsBufferConverter::convertBufferToWxImage(nonemptyBuffer);
+			Assert::AreEqual(2, imageFromEmpty.GetWidth());
+			Assert::AreEqual(3, imageFromEmpty.GetHeight());
+		}
+
+		TEST_METHOD(emptyImage)
+		{
+			{
+				wxImage emptyImage(0, 0);
+				GraphicsBuffer buffer = WxWidgetsBufferConverter::convertWxImageToBuffer(emptyImage);
+				Assert::IsTrue(buffer.isEmpty());
+			}
+			{
+				wxImage emptyImage(0, 1);
+				GraphicsBuffer buffer = WxWidgetsBufferConverter::convertWxImageToBuffer(emptyImage);
+				Assert::IsTrue(buffer.isEmpty());
+			}
+			{
+				wxImage emptyImage(1, 0);
+				GraphicsBuffer buffer = WxWidgetsBufferConverter::convertWxImageToBuffer(emptyImage);
+				Assert::IsTrue(buffer.isEmpty());
+			}
+			{
+				wxImage emptyImage(1, 1);
+				GraphicsBuffer buffer = WxWidgetsBufferConverter::convertWxImageToBuffer(emptyImage);
+				Assert::IsFalse(buffer.isEmpty());
+			}
 		}
 	};
 }
